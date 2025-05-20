f4a0f9f2abdd460a0bd64974276b9a604842742f
Carlos Rodriguez Gomez <carlos061020000@gmail.com>
2025-05-20 23:28:10 +0200

    Added Croos Origin and commets on properties

--- myApp/src/main/java/com/myApp/myApp/controller/ProductController.java
+++ myApp/src/main/java/com/myApp/myApp/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.myApp.myApp.controller;
 import com.myApp.myApp.model.Product;
 import com.myApp.myApp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ProductController {
 
